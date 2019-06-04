c6f941b0a4a962b34bbd5b3416831fd248250e18
JaGaffney <jagaffney661@msn.com>
2019-06-04 14:49:39 +1000

    Updates

---
+++ precache-manifest.2b2f9f8462b59f65f84cc354f7a9a5f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05b0a846387aa9cc3e77a398bd89e19",
+    "url": "/JaGaffney/react-css-playground/index.html"
+  },
+  {
+    "revision": "823fd4e3d328806392c8",
+    "url": "/JaGaffney/react-css-playground/static/css/main.a4e8b079.chunk.css"
+  },
+  {
+    "revision": "a2c8a80b70b05829d7d5",
+    "url": "/JaGaffney/react-css-playground/static/js/2.8a4eca8d.chunk.js"
+  },
+  {
+    "revision": "823fd4e3d328806392c8",
+    "url": "/JaGaffney/react-css-playground/static/js/main.c8778fde.chunk.js"
+  },
+  {
+    "revision": "e623fb60bbc6356d2923",
+    "url": "/JaGaffney/react-css-playground/static/js/runtime~main.2a7c7f68.js"
+  }
+]);
