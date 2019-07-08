582aff08a214e46795df0876b76753c7174985e2
JaGaffney <jagaffney661@msn.com>
2019-07-08 13:46:46 +1000

    Updates

---
+++ precache-manifest.2abdcef206a58656dd2c1cbff363665a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffaf77c2ce2f17d3de6e66d50469a8a8",
+    "url": "/react-css-playground/index.html"
+  },
+  {
+    "revision": "494b9703ac2e0d59de0a",
+    "url": "/react-css-playground/static/css/main.758adc14.chunk.css"
+  },
+  {
+    "revision": "c055c285c18b49025d8d",
+    "url": "/react-css-playground/static/js/2.773e9eb8.chunk.js"
+  },
+  {
+    "revision": "494b9703ac2e0d59de0a",
+    "url": "/react-css-playground/static/js/main.865bfe40.chunk.js"
+  },
+  {
+    "revision": "090e8bd2ca22d6ade372",
+    "url": "/react-css-playground/static/js/runtime~main.2fc70888.js"
+  },
+  {
+    "revision": "793614020df6f3885d8f2091a50df925",
+    "url": "/react-css-playground/static/media/czas.79361402.jpg"
+  },
+  {
+    "revision": "147a6686331052f3f07b710c606e5249",
+    "url": "/react-css-playground/static/media/image3.147a6686.jpg"
+  },
+  {
+    "revision": "bf5bbfdf18412db63f587a67e021113f",
+    "url": "/react-css-playground/static/media/jan.bf5bbfdf.jpg"
+  },
+  {
+    "revision": "5131b2a082f20ef09b45d3cf75148f82",
+    "url": "/react-css-playground/static/media/mechano.5131b2a0.jpg"
+  },
+  {
+    "revision": "550ea0e1a66ee7b2e69184c3ef68b9b5",
+    "url": "/react-css-playground/static/media/mondrian.550ea0e1.jpg"
+  },
+  {
+    "revision": "ccad8da6438173d6a00f971589d60bc1",
+    "url": "/react-css-playground/static/media/monopoly.ccad8da6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-css-playground/precache-manifest.8594a32e992985aa47c4cc970dede8aa.js"
+  "/react-css-playground/precache-manifest.2abdcef206a58656dd2c1cbff363665a.js"
 );
 
 self.addEventListener('message', (event) => {
